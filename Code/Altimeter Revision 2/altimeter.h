6d9a508f1794225395e781d0ebd49abbe8f7aee3
unknown <gisellegk@gmail.com>
2019-12-02 11:40:43 -0700

    abstract altimeter data requests

---
+++ Code/Altimeter Revision 2/altimeter.h
@@ -0,0 +1,32 @@
+/*
+ * altimeter.h
+ *
+ *  Created on: Dec 2, 2019
+ *      Author: Giselle
+ */
+
+
+
+
+#ifndef ALTIMETER_H_
+#define ALTIMETER_H_
+
+#include "msp.h"
+#include "I2C.h"
+#include <stdlib.h>
+
+#define ICP10111 0x63
+#define WRITE_HEADER 0xC6
+#define PRESSURE_MEASUREMENT_MSB 0x48
+#define PRESSURE_MEASUREMENT_LSB 0xA3
+#define READ_HEADER 0xC7
+#define ID_MSB 0xEF
+#define ID_LSB 0xC8
+
+uint8_t request_id(void);
+
+void request_pressure_measurement(uint8_t *pressure);
+void request_full_measurement(uint8_t *data);
+
+
+#endif /* ALTIMETER_H_ */
--- Code/Altimeter Revision 2/main.c
+++ Code/Altimeter Revision 2/main.c
@@ -3,13 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define ICP10111 0x63
-#define WRITE_HEADER 0xC6
-#define PRESSURE_MEASUREMENT_MSB 0x48
-#define PRESSURE_MEASUREMENT_LSB 0xA3
-#define READ_HEADER 0xC7
-#define ID_MSB 0xEF
-#define ID_LSB 0xC8
+#include "altimeter.h"
 
 int main(void)
 {
@@ -17,30 +11,15 @@ int main(void)
 
     config_i2c();
 
-    measurement_sequence(ICP10111, WRITE_HEADER, ID_MSB, ID_LSB);
+    uint8_t id = request_id();
 
-    read_probing(ICP10111, READ_HEADER);
+    uint8_t pressure[6];
 
-    uint8_t i = 0;
-    for (i; i < 2; i++) {
-        uint8_t id = read_data(ICP10111);
+    request_pressure_measurement(pressure);
+
+    while(1){
+
     }
-
-    send_stop();
-
-    measurement_sequence(ICP10111, WRITE_HEADER, PRESSURE_MEASUREMENT_MSB, PRESSURE_MEASUREMENT_LSB);
-
-    read_probing(ICP10111, READ_HEADER);
-
-    uint8_t pressure [6];
-
-    i = 0;
-
-    for(i = 0; i < 6; i++) {
-        pressure[i] = read_data(ICP10111);
-    }
-
-    send_stop();
 
     return 0;
 }
